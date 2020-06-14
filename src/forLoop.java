6d1913aab47f8174a2b0d92fcebe69f902085eae
Kaung-Htet <kaunghtetnyilay@gmail.com>
2020-06-14 11:50:56 +0700

    initial commit

---
+++ src/forLoop.java
@@ -0,0 +1,16 @@
+public class forLoop {
+    public static void main(String[] args) {
+        int amount = 10_000;
+        //for (init, termination, increment)
+        for (int i = 2; i <= 8; i++){
+            //String.format("%.2f") က ဒဿမ ၂ လုံးဖြတ်တာ
+            System.out.println(amount + " at " + i + "% interest = " +
+                    String.format("%.2f",calculateInterest(amount,i)));
+        }
+    }
+
+    public static double calculateInterest (double amount, double interestRate){
+            return  (amount * (interestRate/100));
+
+    }
+}
